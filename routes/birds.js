8532236fdfbd6375b23066bf3c536d0dad00ce4d
MaltheFriisberg <Malthef@gmail.com>
2016-04-12 10:49:56 +0100

    the bird route is now protected by tokenVerification middleware in a seperate file

--- routes/birds.js
+++ routes/birds.js
@@ -1,16 +1,19 @@
 var expresss = require('express');
 var birdRoutes = expresss.Router();
+var tokenVerifier = require('../middlewares/tokenVerification.js');
 
 // middleware that is specific to this router
-birdRoutes.use(function timeLog(req, res, next) {
+/*birdRoutes.use(function timeLog(req, res, next) {
     console.log("birdroutes");
     console.log('Time: ', Date.now());
     next();
-});
+});*/
+
 // define the home page route
 birdRoutes.get('/', function (req, res) {
     res.json({ success: false, message: 'Hi from the birdRoute' });
 });
+birdRoutes.use(tokenVerifier.tokenHandler);
 // define the about route
 birdRoutes.get('/about', function (req, res) {
     res.json({ success: false, message: '/About a birds' });
