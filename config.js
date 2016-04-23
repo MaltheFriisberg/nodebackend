127d22af7e2f775b763f753d013729b7e63b3589
MaltheFriisberg <Malthef@gmail.com>
2016-04-23 18:34:24 +0200

    refactor

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    //Random guid generated in C#
-    'secret': '2b1c8fac-7798-4869-80b9-887b3c8849aa',
-    //shell: mongo ds023560.mlab.com:23560/athleteunbound -u admin -p hejhej123
-    //mongodb://localhost:27017/athleteunbound
-    'database': 'mongodb://admin:hejhej123@ds023560.mlab.com:23560/athleteunbound'
-    
-
-};
